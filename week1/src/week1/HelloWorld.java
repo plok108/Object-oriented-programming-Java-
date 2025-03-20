23a1c9a31079094a1ac4a627e589c216d4a8fba4
plok108 <plok108@naver.com>
2025-03-21 00:39:45 +0900

    first study

---
+++ week1/src/week1/HelloWorld.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("학과 : 컴퓨터정보과");
+		System.out.println("학번 : 202244030");
+		System.out.println("이름 : 정태완");
+	}
+}
