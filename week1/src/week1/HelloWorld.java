51f26f79a101871bf3c68eb2ef8ef954e9ecda88
plok108 <plok108@naver.com>
2025-06-08 16:07:42 +0900

    add

--- week1/src/week1/HelloWorld.java
+++ week1/src/week1/HelloWorld.java
@@ -2,8 +2,8 @@ package week1;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("학과 : 컴퓨터정보과");
-		System.out.println("학번 : 202244030");
-		System.out.println("이름 : 정태완");
+		System.out.println("학과 : 000000");
+		System.out.println("학번 : 202400000");
+		System.out.println("이름 : 김**");
 	}
 }
